15e8bf8bc0c8daf18540b245c92873b05bc72ada
icstunna <mdelostr@gmail.com>
2016-05-15 17:03:45 -0700

    created portfolios controller and model

---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,6 @@
+class PortfoliosController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160513023959) do
+ActiveRecord::Schema.define(version: 20160515235757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "positions", force: :cascade do |t|
     t.string   "ticker"
