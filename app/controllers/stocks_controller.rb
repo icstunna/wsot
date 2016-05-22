ab297df98a2b08b4aa7e4eb00207c1bf1c115eac
icstunna <mdelostr@gmail.com>
2016-05-22 16:01:15 -0700

    stock prices may now be updated
    stock price api is necessary to update prices

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -13,7 +13,21 @@ class StocksController < ApplicationController
     redirect_to stocks_url
   end
 
+  def edit
+    find_models
+  end
+
+  def update
+    find_models
+    @stock.update_attributes(stock_params)
+    redirect_to stocks_url
+  end
+
   private
+
+  def find_models
+    @stock = Stock.find(params[:id])
+  end
 
   def stock_params #scary internet people
     params.require(:stock).permit(:ticker_symbol, :company_name, :price)
