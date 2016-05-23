701dbbaea27a45c854862c37fa657163dfec97cf
icstunna <mdelostr@gmail.com>
2016-05-22 17:55:25 -0700

    portfolio now displays available cash for trading

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,12 @@
 class Portfolio < ActiveRecord::Base
   has_many :positions, dependent: :destroy
   has_many :stocks, :through => :positions
+
+  def available_cash
+    tradeable_cash = cash
+    self.positions.each do |position|
+      tradeable_cash = tradeable_cash - position.value
+    end
+    tradeable_cash
+  end
 end
