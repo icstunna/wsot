c935ef3b93f01bb99861a57e3e339072985d56c0
icstunna <mdelostr@gmail.com>
2016-05-22 17:18:32 -0700

    presenting position P&L
    research d3 to give visual representation of 
    securities.

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,4 +1,16 @@
 class Position < ActiveRecord::Base
   belongs_to :portfolio
   belongs_to :stock
+
+  def value
+    quantity * open_price
+  end
+
+  def current_value
+    quantity * stock.price
+  end
+
+  def profit_loss
+    short ? -(current_value - value) : current_value - value
+  end
 end
